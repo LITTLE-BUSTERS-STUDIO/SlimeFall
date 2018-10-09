bab2b1a6ef07d8be9a588e0da9e94807946674a0
Marc Gálvez <36281630+optus23@users.noreply.github.com>
2018-10-09 18:28:49 +0200

    Level Design 1 Added (tsx not converted)
    Collision Box + Slime_animation_tile_Blue + Slime_animation_tile_Green+ Slime_animation_tile_Magenta + Tileset + Tree_SlimeFall + wateranimate3 + wateranimate4 + wateranimate5 +wateranimate6

---
+++ FallSlime_LITTLE_BUSTERS/Game/maps/Slime_animation_tile_Magenta.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="Slime_animation_tile_Magenta" tilewidth="33" tileheight="33" tilecount="11" columns="11">
+ <image source="../Slime/Riped/Slime_animation_tile_Magenta.png" width="363" height="33"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="10" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
