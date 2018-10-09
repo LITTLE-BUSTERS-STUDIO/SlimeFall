01f2b38f874d6f7a9f188be6ad1f970a977006f7
AAurelioGN <36158509+alejandro61299@users.noreply.github.com>
2018-10-09 21:42:04 +0200

    Fixed Colliders Bug

--- FallSlime_LITTLE_BUSTERS/Motor2D/j1Collision.cpp
+++ FallSlime_LITTLE_BUSTERS/Motor2D/j1Collision.cpp
@@ -164,9 +164,6 @@ bool j1Collision:: CleanUp()
 	Collider* collider = new Collider(rect, type , callback);
 
 	return colliders.add(collider);
-
-
-
 }
 
 bool Collider::CheckCollision(const SDL_Rect& r) const
