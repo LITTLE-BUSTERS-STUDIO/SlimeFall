2ed43bdacdbdcd593a56b31bd1b17fe6d82fe89a
Marc Gálvez <36281630+optus23@users.noreply.github.com>
2018-10-10 17:24:54 +0200

    MIddle Debug

--- FallSlime_LITTLE_BUSTERS/Motor2D/j1Collision.cpp
+++ FallSlime_LITTLE_BUSTERS/Motor2D/j1Collision.cpp
@@ -132,6 +132,8 @@ bool j1Collision::PostUpdate()
 			break;
 		case COLLIDER_PLAYER: // green
 			App->render->DrawQuad(item->data->rect, 0, 255, 0, alpha);
+			App->render->DrawCircle(640, 360, 1, 0, 255, 0, alpha); // middle of the screen
+
 			break;
 		case COLLIDER_ENEMY: // red
 			App->render->DrawQuad(item->data->rect, 255, 0, 0, alpha);
