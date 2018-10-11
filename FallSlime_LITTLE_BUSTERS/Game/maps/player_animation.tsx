d4a94dc5265376b085bdecb75497ae35ee40973a
Marc Gálvez <36281630+optus23@users.noreply.github.com>
2018-10-12 01:30:11 +0200

    Player animation added tsx

---
+++ FallSlime_LITTLE_BUSTERS/Game/maps/player_animation.tsx
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="player_animation" tilewidth="33" tileheight="33" tilecount="33" columns="11">
+ <image source="player_animation.png" width="363" height="99"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="200"/>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="2" duration="200"/>
+   <frame tileid="3" duration="200"/>
+   <frame tileid="4" duration="200"/>
+   <frame tileid="5" duration="200"/>
+   <frame tileid="6" duration="200"/>
+   <frame tileid="7" duration="200"/>
+   <frame tileid="8" duration="200"/>
+   <frame tileid="9" duration="200"/>
+   <frame tileid="10" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="11">
+  <animation>
+   <frame tileid="11" duration="200"/>
+   <frame tileid="12" duration="200"/>
+   <frame tileid="13" duration="200"/>
+   <frame tileid="14" duration="200"/>
+   <frame tileid="15" duration="200"/>
+   <frame tileid="16" duration="200"/>
+   <frame tileid="17" duration="200"/>
+   <frame tileid="18" duration="200"/>
+   <frame tileid="19" duration="200"/>
+   <frame tileid="20" duration="200"/>
+   <frame tileid="21" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="22">
+  <animation>
+   <frame tileid="22" duration="200"/>
+   <frame tileid="23" duration="200"/>
+   <frame tileid="24" duration="200"/>
+   <frame tileid="25" duration="200"/>
+   <frame tileid="26" duration="200"/>
+   <frame tileid="27" duration="200"/>
+   <frame tileid="28" duration="200"/>
+   <frame tileid="29" duration="200"/>
+   <frame tileid="30" duration="200"/>
+   <frame tileid="31" duration="200"/>
+   <frame tileid="32" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
