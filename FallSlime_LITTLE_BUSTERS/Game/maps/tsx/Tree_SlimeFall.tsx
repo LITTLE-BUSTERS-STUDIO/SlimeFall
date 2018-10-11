abd350d55724e3d3e3eb3b39c24784cc45d69fef
Marc Gálvez <36281630+optus23@users.noreply.github.com>
2018-10-11 21:18:45 +0200

    unnecessary files destroyed

--- FallSlime_LITTLE_BUSTERS/Game/maps/tsx/Tree_SlimeFall.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.0" name="Tree_tileset" tilewidth="16" tileheight="16" tilecount="108" columns="9">
- <image source="Tree.png" width="152" height="196"/>
- <terraintypes>
-        <terrain name="Tree_tileset" tile="0"/>
- </terraintypes>
-</tileset>
